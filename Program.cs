8111caa3615d119b210c3b262239832d81bedcb3
Vovka0777 <94632891+Vovka0777@users.noreply.github.com>
2025-02-06 02:32:15 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp21_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Time time1 = new Time();
+            time1.Info();
+            Console.WriteLine();
+            time1.hours = 3;
+            time1.minutes = time1.hours * 60;
+            time1.seconds = time1.hours * 3600;
+            time1.Info();
+            Console.WriteLine();
+            Time time2 = new Time(3, time1.minutes, time1.seconds);
+            time2.Info();
+            Console.WriteLine();
+            Console.Write("Количество часов = ");
+            int hours = int.Parse(Console.ReadLine());
+            int minutes = hours * 60;
+            int seconds = hours * 3600;
+            Time time3 = new Time(hours, minutes, seconds);
+            time3.Info();
+        }
+    }
+}
---
+++ Time.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp21_1
+{
+    internal class Time
+    {
+        public int hours;
+        public int minutes;
+        public int seconds;
+        public Time() { }
+        public Time(int hours) { this.hours = hours; }
+        public Time (int hours, int minutes) : this(hours) { this.minutes = minutes; }
+        public Time (int hours, int minutes, int seconds) : this(hours,minutes) { this.seconds = seconds; }
+
+        public void Info()
+        {
+            Console.WriteLine($"В {hours} часах - {minutes} минут и {seconds} секунд");
+        }
+
+        public override string ToString()
+        {
+            return $"В {hours} часах - {minutes} минут и {seconds} секунд";
+        }
+    }
+}
